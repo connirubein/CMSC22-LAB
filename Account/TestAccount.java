1f8e93fef0bb0071fd02de760522be6a465cd62c
connirubein <connirubeinbaliog@gmail.com>
2016-09-19 16:49:27 +0800

    Add files via upload

---
+++ Account/TestAccount.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class TestAccount{
+
+   public static void main(String[] args) {
+      // Test constructor and toString()
+      Account a1 = new Account(123, 1000.0);
+      System.out.println(a1);
+      System.out.println(a2); 
+      // Test Setters and Getters
+
+      a1.setBalance(2000.0);
+
+      System.out.println(a1);
+
+      a1.credit(200.0);
+
+      a1.debit(5000.0);
+      
+      a1.debit(500.0);
+
+      System.out.println("Account Number (a1) is: " + a1.getAccountNumber());
+      System.out.println("Balance (a1) is: " + a1.getBalance());
+ 
+   }
+}
