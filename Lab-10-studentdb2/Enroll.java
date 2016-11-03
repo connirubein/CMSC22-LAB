d5a606cf3a8552f1e9864804e182e4f66a2b50c6
connirubein <connirubeinbaliog@gmail.com>
2016-11-04 01:00:25 +0800

    Update Enroll.java

--- Lab-10-studentdb2/Enroll.java
+++ Lab-10-studentdb2/Enroll.java
@@ -15,53 +15,182 @@ import java.io.*;
 
 public class Enroll {
 
-    private static final String FILE_NAME = "testing.txt";
+    //private static final String FILE_NAME = "testing.txt";
 
-    public static void main(String[] args) {
-
-        FileOutputStream fos = null;
-        ObjectOutputStream oos = null;
-        FileInputStream fis = null;
-        ObjectInputStream ois = null;
-
-        List<Student> myList = new ArrayList<Student>();
-        Student student = null;
-        BufferedReader br = null;
-
-        String studentNumber = null;
-        String firstName = null;
-        String lastName = null;
-        String course = null;
-        char middleInitial = 'a';
-        int yearLevel = 0;
-        int count = 0;
-
-        try {
-
-            fis = new FileInputStream(FILE_NAME);
-            ois = new ObjectInputStream(fis);
-
-            String sCurrentLine;
-            br = new BufferedReader(new FileReader("testing.txt"));
-            int counter = 0;
-            for(; ( sCurrentLine = br.readLine() ) != null; counter++);
-            counter /=8;
-
-            while (counter>0) {
-
-                Student s = (Student) ois.readObject();
-
-                myList.add(s);
-
-                counter--;
+    List<Student> lList = new ArrayList<Student>();
+    FileInputStream fis = null;
+    ObjectInputStream ois = null;
+    Student s = null;
+    
+    public static void main(String[] args) throws IOException {
+        
+        Enroll e = new Enroll();
+        
+        e.enrollment();
+        
+    }
+    
+    public void enrollment() throws IOException{
+        
+        try{
+            
+            //private static final String FILE_NAME = "input.txt";
+            fis = new FileInputStream(input.txt);
+            ois = new ObjectInputStream (fis);
+            
+            if(ois.read() == -1){
+                
+                lList = (List<Student>) ois.readObject();
+            
             }
-
-        } catch (IOException m) {
-            m.printStackTrace();
-        } catch (ClassNotFoundException ce) {
+            
+            ois.close();
+        
+        }//try
+        
+        catch(EOFException e){
+            e.printStackTrace();
+        }
+        
+        catch(ClassNotFoundException e){
             // this might be thrown by ois.readObject()
-            ce.printStackTrace();
-        } finally {
+        }
+        
+        int choice;
+        do{
+            boolean studentExists = false;
+            System.out.println("Menu\n\t1. Enroll a Student\n\t2. Search for a Student\n\t3. Unenroll a Student\n\t4. Save\n\t5. Exit");
+            choice = new Scanner(System.in).nextInt();
+            
+            switch (choice) {
+
+                case 1: {
+
+                    System.out.println("Enter Student Number:");
+                    String sNumber = new Scanner(System.in).nextLine();
+                    errorCheck(lList, sNumber);
+                    System.out.println("Enter First Name:");
+                    String fName = new Scanner(System.in).nextLine();
+                    System.out.println("Enter Middle Initial:");
+                    char mInitial = new Scanner(System.in).next(".").charAt(0);
+                    System.out.println("Enter Last Name:");
+                    String lName = new Scanner(System.in).nextLine();
+                    System.out.println("Enter Program:");
+                    String program = new Scanner(System.in).nextLine();
+                    System.out.println("Enter Year Level:");
+                    int yrLevel = new Scanner(System.in).nextInt();
+                    System.out.println("Crush's Name:");
+                    String cName = new Scanner(System.in).nextLine();
+                    System.out.println("Favorite Subject:");
+                    System.out.print("\tCourse Code:");
+                    String cCode = new Scanner(System.in).nextLine();
+                    System.out.print("\n\tCourse Description:");
+                    String cDescription = new Scanner(System.in).nextLine();
+                    System.out.println();
+
+                    s = new Student(sNumber, fName, mInitial, lName, program, yrLevel, cName, cCode, cDescription);
+                    lList.add(s);
+                    System.out.println("> Enrolled a student <");
+                    break;
+                }
+
+                case 2: {
+
+                    System.out.println("Look for a Student:");
+                    String sNumber2 = new Scanner(System.in).nextLine();
+
+                    for(Student ss: lList) {
+                        
+                        if(ss.getStudentNumber().equals(sNumber2)){
+                            System.out.println("Student Number: " + ss.getStudentNumber() );
+                            System.out.println("Name: " + ss.getLastName() + ", " + ss.getFirstName() + " " + ss.getMiddleInitial() + ".");
+                            System.out.println("Program: " + ss.getCourse() );
+                            System.out.println("Year Level: " + ss.getYearLevel() );
+                            System.out.println("Crush's Name: " + ss.getCrushName() );
+                            System.out.println("Favorite Subject:");
+                            System.out.println("\tCourse Code: " + ss.getCourseCode() );
+                            System.out.println("\tCourse Description: " + ss.getCourseDescription() );
+                            studentExists = true;
+                            break;
+                        }
+                        
+                    }
+                    
+                    if(studentExists == false){
+                        
+                        System.out.println("> Student is not enrolled <");
+                    
+                    }
+
+                    break;
+                }   //case 2
+
+                case 3: {
+                
+                    System.out.println("Unenroll a Student:");
+                    String sNumber2 = new Scanner(System.in).nextLine();
+                    for(Student ss: lList) {
+                        if(ss.getStudentNumber().equals(sNumber2)){
+                            lList.remove(ss);
+                            System.out.println("Successful! Unenrolled Student with Student Number: " + s.getStudentNumber() );
+                            studentExists = true;
+                            break;
+                        }
+                    }
+                
+                    if(studentExists == false){
+                
+                        System.out.println("Error. Student wasn't enrolled in the first place :(");
+                    
+                    }
+                    
+                    break;
+
+                }       //case 3
+
+                case 4: {
+                
+                    try {
+                        
+                        //serialize
+                        FileOutputStream fos = new FileOutputStream("input.txt");
+                        ObjectOutputStream oos = new ObjectOutputStream(fos);
+                        
+                        oos.writeObject(lList);
+                        oos.close();
+                        fos.close();
+                        
+                        System.out.println("> Save Successful <");
+                        
+                    }
+                    
+                    catch(IOException e){
+                
+                        e.printStackTrace();
+                    
+                    }
+                
+                    break;
+                
+                }       //end of case 4
+
+                case 5: {
+                    
+                    System.out.println("Exit");
+                    
+                    break;
+                
+                }
+                
+                default:
+                
+                    throw new IllegalArgumentException("> Invalid <");
+            
+            }   //switch
+            
+        }while(choice != 5);
+        
+        finally {
 
             // make sure to close the files!
             try {
@@ -70,138 +199,17 @@ public class Enroll {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
+        }   //finally
+        
+    }       //enrollment
 
-        int choice;
-        do{
-            System.out.println("\t\tMenu\n\t1. Register Student\n\t2. Search Student\n\t3. Delete Student\n\t4. Save Student\n\t5. Exit\n");
-            choice = new Scanner(System.in).nextInt();
-            boolean b = false;
-
-            switch (choice) {
-
-                case 1: {
-
-                    System.out.println("Student Number:");
-                    String isStudentNumber = new Scanner(System.in).nextLine();
-                    check(myList, isStudentNumber);
-
-                    System.out.println("First Name:");
-                    String isFirstName = new Scanner(System.in).nextLine();
-
-                    System.out.println("Middle Initial:");
-                    char isMiddleInitial = new Scanner(System.in).next(".").charAt(0);
-
-                    System.out.println("Last Name:");
-                    String isLastName = new Scanner(System.in).nextLine();
-
-                    System.out.println("Degree Program:");
-                    String isCourse = new Scanner(System.in).nextLine();
-
-                    System.out.println("Year Level:");
-                    int isYearLevel = new Scanner(System.in).nextInt();
-
-                    System.out.println("Crush's Name:");
-                    String isCrushName = new Scanner(System.in).nextLine();
-
-                    System.out.println("Favorite Subject:");
-                    System.out.print("Course Code:");
-                    String isCourseCode = new Scanner(System.in).nextLine();
-                    System.out.print("\nCourse Description:");
-                    String isCourseDescription = new Scanner(System.in).nextLine();
-                    System.out.println();
-
-
-                    student = new Student(isStudentNumber, isFirstName, isMiddleInitial, isLastName, isCourse, isYearLevel, isCrushName, isCourseCode, isCourseDescription);
-                    myList.add(student);
-                    System.out.println("\nSTUDENT ADDED!");
-                    break;
-                }
-
-                case 2: {
-
-                    System.out.println("Search Student:");
-                    String searchStudentNumber = new Scanner(System.in).nextLine();
-
-                    for(Student s: myList) {
-                        if(s.getStudentNumber().equals(searchStudentNumber)){
-                            System.out.println("Student Number: " + s.getStudentNumber() + "\n"
-                                    + "Name: " + s.getLastName() + ", " + s.getFirstName() + " " + s.getMiddleInitial() + "." + "\n"
-                                    + "Program: " + s.getCourse() + "\n"
-                                    + "Year Level: " + s.getYearLevel() + "\n");
-                            b = true;
-                            break;
-                        }
-                    }
-                    if(b == false){
-                        System.out.println("Student does not exist!\n");
-                    }
-
-                    break;
-                }
-
-                case 3: {
-                    System.out.println("Delete Student:");
-                    String deleteStudent = new Scanner(System.in).nextLine();
-                    for(Student s: myList) {
-                        if(s.getStudentNumber().equals(deleteStudent)){
-                            myList.remove(s);
-                            System.out.println("\nStudent " + s.getStudentNumber() + " deleted!\n");
-                            b = true;
-                            break;
-                        }
-                    }
-                    if(b == false){
-                        System.out.println("Cannot remove student. No such student found.\n");
-                    }
-                    break;
-
-                }
-
-                case 4: {
-                    try {
-                        File file = new File("testing.txt");
-                        if (!file.exists()) {
-                            file.createNewFile();
-                        }
-
-                        // serialize...
-                        fos = new FileOutputStream(FILE_NAME);
-                        oos = new ObjectOutputStream(fos);
-
-                        //FileWriter fw = new FileWriter(file);
-                        //BufferedWriter bw = new BufferedWriter(fw);
-                        for(Student s: myList) {
-                            oos.writeObject(s);
-                            oos.writeObject("\n");
-                        }
-                        //bw.close();
-
-                        oos.close();
-
-                        System.out.println("Saved!\n");
-
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-
-                }       //end of case 4
-
-                case 5: {
-                    System.out.println("Exit!");
-                    break;
-                }
-                default:
-                    throw new IllegalArgumentException("Invalid input!\n");
-            }
-        }while(choice != 5);
-
-    }
-    private static void check(List<Student> myList, String studentNumber){
-        for (Student s: myList){
+       // FileOutputStream fos = null;
+       // ObjectOutputStream oos = null;
+        
+    private static void errorCheck(List<Student> lList, String studentNumber){
+        for (Student s: lList){
             if(s.getStudentNumber().equals(studentNumber)){
-                throw new IllegalArgumentException("Student already exist!");
+                throw new IllegalArgumentException("This student already exists!");
             }
         }
     }
