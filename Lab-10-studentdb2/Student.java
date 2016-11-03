e1890b693ae4e8abb42a2779af1fe2997e0b9a02
connirubein <connirubeinbaliog@gmail.com>
2016-11-04 01:09:00 +0800

    Update Student.java

--- Lab-10-studentdb2/Student.java
+++ Lab-10-studentdb2/Student.java
@@ -14,8 +14,7 @@ public class Student implements Serializable{
     private String crushName;
     private Course faveSubject;
 
-    public Student(String studentNumber, String firstName, char middleInitial, String lastName, String course,
-                   int yearLevel, String crushName, String courseCode, String courseDescription) {
+    public Student(String studentNumber, String firstName, char middleInitial, String lastName, String course, int yearLevel, String crushName, String courseCode, String courseDescription) {
         super();
         this.studentNumber = studentNumber;
         this.firstName = firstName;
@@ -26,64 +25,123 @@ public class Student implements Serializable{
         this.crushName = crushName;
         this.setFaveSubject(courseCode,courseDescription);
     }
+    
     public String getStudentNumber() {
+    
         return studentNumber;
+    
     }
+    
     public String getCrushName() {
+    
         return crushName;
+    
     }
+    
+    public void setCrushName(String cName) {
+    
+        this.crushName = cName;
+    
+    }
+    
     public void setFaveSubject(String cc, String cd) {
+    
         this.faveSubject.setCourseCode(cc);
+        
         this.faveSubject.setCourseDescription(cd);
+    
     }
+    
     public Course getFaveSubject() {
+    
         //Course c = new Course();
         //c.setCourseCode(this.getCourseCode());
         //c.setCourseDescription(this.getCourseDescription());
         return faveSubject;
+    
     }
+    
     public String getCourseCode() {
+    
         return faveSubject.getCourseCode();
+    
     }
+    
     public String getCourseDescription() {
+    
         return faveSubject.getCourseDescription();
+    
     }
+    
     public void setStudentNumber(String studentNumber) {
+    
         this.studentNumber = studentNumber;
+   
     }
+    
     public String getFirstName() {
+    
         return firstName;
+    
     }
+    
     public void setFirstName(String firstName) {
+    
         this.firstName = firstName;
+    
     }
+    
     public char getMiddleInitial() {
+    
         return middleInitial;
+    
     }
+    
     public void setMiddleInitial(char middleInitial) {
+    
         this.middleInitial = middleInitial;
+    
     }
+    
     public String getLastName() {
+    
         return lastName;
+    
     }
+    
     public void setLastName(String lastName) {
+    
         this.lastName = lastName;
+    
     }
+    
     public String getCourse() {
+    
         return course;
+    
     }
+    
     public void setCourse(String course) {
+    
         this.course = course;
+    
     }
+    
     public int getYearLevel() {
+    
         return yearLevel;
+    
     }
+    
     public void setYearLevel(int yearLevel) {
         this.yearLevel = yearLevel;
     }
+    
+    
+    
     @Override
     public String toString() {
-        return String.format("Student Number: %s\nFirst Name: %s\nMiddle Initial: %c\nLast Name: %s\nDegree Program: %s\nYear Level: %d\nCrush's Name: %s\nFavorite Subject: %s, %s", studentNumber, firstName, middleInitial, lastName, course, yearLevel, crushName, getCourseCode(), getCourseDescription());
+        return String.format("Student Number: %s\nFirst Name: %s\nMiddle Initial: %c\nLast Name: %s\nProgram: %s\nYear Level: %d\nCrush's Name: %s\nFavorite Subject: \n\tCourse code: %s\n\tCourse Description: %s" studentNumber, firstName, middleInitial, lastName, course, yearLevel, crushName, getCourseCode(), getCourseDescription());
     }
 
 
